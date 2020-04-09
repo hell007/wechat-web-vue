6a79cc1fe9d1ff6767d3b08272d3ae2053d85c2f
hell007 <1048523306@qq.com>
2020-04-09 14:24:44 +0800

    vm vh 移动端

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,15 @@ module.exports = {
 	plugins: [
 		require('autoprefixer')({
 			browsers: ['last 100 versions']
-		})
+		}),
+		'postcss-px-to-viewport': {
+			viewportWidth: 375, // 视窗的宽度，设计稿的宽度
+			viewportHeight: 667, //视窗的高度，也可以不配置
+			unitPrecision: 3, // 指定`px`转换为视窗单位值的小数位数
+			viewportUnit: "vw", //指定需要转换成的视窗单位，建议使用vw
+			selectorBlackList: ['.ignore'], // 指定不转换为视窗单位的类，可以自定义，可以无限添加,建议定义一至两个通用的类名
+			minPixelValue: 1, // 小于或等于`1px`不转换为视窗单位，你也可以设置为你想要的值
+			mediaQuery: false // 允许在媒体查询中转换`px`
+		}
 	]
 };
